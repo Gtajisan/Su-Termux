df4fc71d9d8462fdc140ceb06863f6ad96a04d3e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-12-09 13:25:04 +0600

    Update su.py

--- su.py
+++ su.py
@@ -8,26 +8,26 @@ if sys.platform in ["linux","linux2"]:
 	R = ("\033[31;1m")
 '''Script'''
 '''banner'''
-banner = (G+" .d8888. d888888b \n 88'  YP `~~88~~' "+R+"Tools : su-Termux\n"+G+" `8bo.      88    "+R+"Version : 1.0\n"+G+"   `Y8b.    88    "+R+"Author : FARHAN-MUH-TASIM\n"+G+" db   8D    88    "+R+"https://github.com/Gtajisan\n"+G+" `8888Y'    YP    ")
+banner = (G+" .d8888. d888888b \n 88'  YP `~~88~~' "+R+"Tools : Su-Termux\n"+G+" `8bo.      88    "+R+"Version : 1.0\n"+G+"   `Y8b.    88    "+R+"Author : FARHAN-MUH-TASIM\n"+G+" db   8D    88    "+R+"https://github.com/Gtajisan\n"+G+" `8888Y'    YP    ")
 def xnchan():
  try:
     print (R+"-----------------------------------------")
     print (banner)
     print (R+"-----------------------------------------")
-    print (G+"[+] installing su-termux...\n")
+    print (G+"[+] installing sudo-termux...\n")
     sleep(3)
     os.system("apt install ncurses-utils")
     sleep(3)
     u = urllib2
-    site = ("https://github.com/Gtajisan/Su-Termux/raw/master/su")
+    site = ("https://raw.githubusercontent.com/Gtajisan/termux-sudo/master/sudo")
     u.Request(site)
     r = u.urlopen(site)
     c = r.read()
-    file = open("/data/data/com.termux/files/usr/bin/su","w")
+    file = open("/data/data/com.termux/files/usr/bin/sudo","w")
     file.write(c)
-    os.chmod("/data/data/com.termux/files/usr/bin/su", 0700)
-    print (G+"\n[+] Success install su-termux")
+    os.chmod("/data/data/com.termux/files/usr/bin/sudo", 0700)
+    print (G+"\n[+] Success install sudo-termux")
  except:
-    print (R+"\n[!] Failed install su-termux")
+    print (R+"\n[!] Failed install sudo-termux")
 if __name__ == "__main__":
 	xnchan()
